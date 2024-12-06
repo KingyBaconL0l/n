989c651c4dfaf93844025bd0d8f64bc9f4e1924f
KingyBaconL0l <87640209+KingyBaconL0l@users.noreply.github.com>
2024-12-06 22:43:06 +0500

    Add files via upload

---
+++ Новая папка/CHAT-Client/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHAT_Client
+{
+    internal class Program
+    {
+        private static bool VerifyServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            var hash = certificate.GetCertHashString();
+            if (hash == "A52C43D7DB126B69DEA14CE6C6260FF0A1C951EF")
+            {
+                Console.WriteLine($"[+] Accepted server certificate with hash {hash}");
+                return true;
+            }
+
+            Console.WriteLine($"[-] Unknown server certificate, rejecting.\r\n" +
+                $"    Hash: {hash}\r\n" +
+                $"    Subject: {certificate.Subject}\r\n" +
+                $"    Issuer: {certificate.Issuer}");
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            string serverAddress = "127.0.0.1";
+            int serverPort = 443;
+
+            try
+            {
+                var tcpClient = new TcpClient(serverAddress, serverPort);
+
+                var sslStream = new SslStream(tcpClient.GetStream(), false,
+                    new RemoteCertificateValidationCallback(VerifyServerCertificate));
+
+                // we dont send any certificates as we dont perform mutual authentication
+                sslStream.AuthenticateAsClient(serverAddress, new X509CertificateCollection(),
+                    System.Security.Authentication.SslProtocols.Tls12, false);
+
+                Console.WriteLine($"[+] Connected to {tcpClient.Client.RemoteEndPoint}");
+
+                byte[] buffer = new byte[2048];
+                AsyncCallback readCallback = null;
+                sslStream.BeginRead(buffer, 0, buffer.Length, readCallback = (ar) => {
+                    try
+                    {
+                        var bytesRead = sslStream.EndRead(ar);
+                        var response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        Console.Write(response);
+
+                        // using Task to avoid stack overflow from recursive calling
+                        Task.Run(() =>
+                            sslStream.BeginRead(buffer, 0, buffer.Length, readCallback, null));
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"[-] Error reading from stream: {ex.Message}");
+                        sslStream.Dispose();
+                        tcpClient.Dispose();
+                    }
+                }, null);
+
+                Console.CancelKeyPress += (o,e) => {
+                    sslStream.Dispose();
+                    tcpClient.Dispose();
+                };
+
+                while (true)
+                {
+                    var strMessage = Console.ReadLine() + "\r\n"; // ReadLine() returns string without new line
+                    var byteMessage = Encoding.UTF8.GetBytes(strMessage);
+                    sslStream.Write(byteMessage);
+                    sslStream.Flush();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[-] Error: " + ex.Message);
+            }
+        }
+
+    }
+}
---
+++ Новая папка/CHAT-Server/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+using System.Net.Http;
+
+namespace CHAT_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var certificatePassword = "certPasswd";
+            // loading certificate from memory, as we want server to be single executable without dependencies
+            var certificate = new X509Certificate2(Resources.cert, certificatePassword);
+
+            var listener = new TcpListener(IPAddress.Loopback, 443);
+            listener.Start();
+            Console.WriteLine($"[*] Listening for connection on {listener.LocalEndpoint}");
+
+            var tcpClient = listener.AcceptTcpClient();
+            Console.WriteLine($"[+] {tcpClient.Client.RemoteEndPoint} connected.");
+
+            // we dont support multiple clients, so there's no need in keeping to listen for clients
+            listener.Stop();
+
+            var sslStream = new SslStream(tcpClient.GetStream(), false,
+                new RemoteCertificateValidationCallback(
+                    (sender, receivedCertificate, chain, sslPolicyErrors) => true // we don't perform mutual authentication, so skip verification of client's cert
+            )); 
+
+            try
+            {
+                sslStream.AuthenticateAsServer(certificate, false, System.Security.Authentication.SslProtocols.Tls12, true);
+
+                byte[] buffer = new byte[2048];
+                AsyncCallback readCallback = null;
+                sslStream.BeginRead(buffer, 0, buffer.Length, readCallback = (ar) => {
+                    try
+                    {
+                        var bytesRead = sslStream.EndRead(ar);
+                        var response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        Console.Write(response);
+
+                        // using Task to avoid stack overflow from recursive calling
+                        Task.Run(() =>
+                            sslStream.BeginRead(buffer, 0, buffer.Length, readCallback, null));
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"[-] Error reading from stream: {ex.Message}");
+                        sslStream.Dispose();
+                        tcpClient.Dispose();
+                    }
+                }, null);
+
+                Console.CancelKeyPress += (o, e) => {
+                    sslStream.Dispose();
+                    tcpClient.Dispose();
+                };
+
+                while (true)
+                {
+                    var strMessage = Console.ReadLine() + "\r\n"; // ReadLine() returns string without new line
+                    var byteMessage = Encoding.UTF8.GetBytes(strMessage);
+                    sslStream.Write(byteMessage);
+                    sslStream.Flush();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[-] Error: {ex.Message}");
+            }
+        }
+    }
+}
